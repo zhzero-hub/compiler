1b7368bcff875ca55be8592a94ecba1d8af563b4
zhzero <545033805@qq.com>
2020-10-24 22:06:04 +0800

    2020.10.24 22:06:04

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ git init
 git add -A
 if [ $1x = x ]    #无commit信息，自动commit时间
 then
-    git commit -m "`date +"%Y-%m-%d %H:%M:%S"`"
+    git commit -m "`date +"%Y.%m.%d %H:%M:%S"`"
 
 else
     git commit -m "$1"
