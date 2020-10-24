07bc8b74938f29ba3ee6251aef16e42c930d1bed
zhzero <545033805@qq.com>
2020-10-24 21:58:30 +0800

    gitshell

---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git init
+git add -A
+if [ $1x = x ]    #无commit信息，自动commit时间
+then
+    git commit -m "`date +"%Y-%m-%d %H:%M:%S"`"
+
+else
+    git commit -m "$1"
+fi
+
+git push origin main
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time=`date +"%Y-%m-%d%H:%M:%S"`
+echo "$time"
